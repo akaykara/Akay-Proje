926daf722ca92ecee6896eb668e380b04b6f1f14
akaykara <akay_kara@hotmail.com>
2014-06-07 12:00:07 +0300

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,2 +1,16 @@
 console.log('hello! !!');
 phantom.exit();
+
+
+var page = require("webpage").create();
+page.open("http://localhost:63342/BespokeCloud/app/index.html")
+    .then(function(status){
+        if (status == "success") {
+            console.log("The title of the page is: "+ page.title);
+        }
+        else {
+            console.log("Sorry, the page is not loaded");
+        }
+        page.close();
+        phantom.exit();
+    })
