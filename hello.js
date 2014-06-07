d79653fc4d1c0bc61e4db13b8af04475ba0fa457
akaykara <akay_kara@hotmail.com>
2014-06-07 12:11:05 +0300

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,12 +1,16 @@
-var page = require("webpage").create();
-page.open("https://www.facebook.com/")
-    .then(function(status){
-        if (status == "success") {
-            console.log("The title of the page is: "+ page.title);
-        }
-        else {
-            console.log("Sorry, the page is not loaded");
-        }
-        page.close();
-        phantom.exit();
-    })
+
+console.log('hello! !!');
+phantom.exit();
+
+//var page = require("webpage").create();
+//page.open("https://www.facebook.com/")
+//    .then(function(status){
+//        if (status == "success") {
+  //          console.log("The title of the page is: "+ page.title);
+    //    }
+      //  else {
+//            console.log("Sorry, the page is not loaded");
+  //      }
+    //    page.close();
+      //  phantom.exit();
+    //})
