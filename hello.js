d6a55eaa2c9e75dcde995a86b9de21b9b9d2e03f
akaykara <akay_kara@hotmail.com>
2014-06-25 13:43:41 +0300

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,16 +1,16 @@
 
-console.log('hello! !! test!!!!.....');
-phantom.exit();
+//console.log('hello! !! test!!!!.....');
+//phantom.exit();
 
-//var page = require("webpage").create();
-//page.open("https://www.facebook.com/")
-//    .then(function(status){
-//        if (status == "success") {
-  //          console.log("The title of the page is: "+ page.title);
-    //    }
-      //  else {
-//            console.log("Sorry, the page is not loaded");
-  //      }
-    //    page.close();
-      //  phantom.exit();
-    //})
+var page = require("webpage").create();
+page.open("http://www.yearofmoo.com/2013/01/full-spectrum-testing-with-angularjs-and-karma.html")
+    .then(function(status){
+        if (status == "success") {
+            console.log("The title of the page is: "+ page.title);
+        }
+        else {
+            console.log("Sorry, the page is not loaded");
+        }
+        page.close();
+        phantom.exit();
+    })
