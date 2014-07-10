7c5a896241e9fe0b99e8dadce06ab5721d21c73e
akaykara <akay_kara@hotmail.com>
2014-07-10 13:47:30 +0300

    Update ptor-conf.js

--- ptor-conf.js
+++ ptor-conf.js
@@ -5,7 +5,8 @@
 
 exports.config = {
 	// The address of a running selenium server.
-	seleniumAddress : 'http://localhost:4444/wd/hub',
+//	seleniumAddress : 'http://localhost:4444/wd/hub',
+	seleniumAddress : 'http://127.0.0.1:4444/wd/hub',
 
 	// Capabilities to be passed to the webdriver instance.
 	capabilities : {
