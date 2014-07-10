91cdcbe1d334a832a7a76b230eda27bbe83ce191
akaykara <akay_kara@hotmail.com>
2014-07-10 16:17:22 +0300

    Update ptor-conf.js

--- ptor-conf.js
+++ ptor-conf.js
@@ -10,7 +10,7 @@ exports.config = {
 
 	// Capabilities to be passed to the webdriver instance.
 	capabilities : {
-		'browserName' : 'firefox'
+		'browserName' : 'google-chrome-beta-'
 	},
 
    /* customLaunchers: {
