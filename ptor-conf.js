7c640abe7f9ea52722811147e17b9947606270db
akaykara <akay_kara@hotmail.com>
2014-08-08 18:32:59 +0300

    Update ptor-conf.js

--- ptor-conf.js
+++ ptor-conf.js
@@ -3,43 +3,44 @@
  * Patched by Akay 4/22/14
  */
 
+
 exports.config = {
 	// The address of a running selenium server.
-//	seleniumAddress : 'http://localhost:4444/wd/hub',
-	seleniumAddress : 'http://188.226.206.224:4444/wd/hub',
+	seleniumAddress : 'http://localhost:4444/wd/hub',
+    //seleniumAddress : 'http://188.226.206.224:4444/wd/hub',
 
-	// Capabilities to be passed to the webdriver instance.
+    // Capabilities to be passed to the webdriver instance.
 	capabilities : {
-		'browserName' : 'google-chrome-beta'
+		'browserName' : 'firefox'
 	},
 
-   /* customLaunchers: {
-        chrome_without_security: {
-            base: 'Chrome',
-            flags: ['--disable-web-security']
-        }
-    },*/
-
 	//If true, only chromedriver will be started, not a standalone selenium.
-	chromeOnly : true,
+	chromeOnly : false,
 
-	// Spec patterns are relative to the current working directly when
+   //Setting of the window size.
+    onPrepare: function() {
+        browser.driver.manage().window().setSize(1350, 600);
+    },
+
+    // Spec patterns are relative to the current working directly when
 	// protractor is called.
 
-	specs : ['./test_login_failure.js'],
+	specs : ['./e2e/protractor/test_draw.js','./e2e/protractor/jenkins_test.js'],
 
+    //specs : ['./e2e/angular_ex.js'],
 
 	baseUrl : 'http://localhost:63342/BespokeCloud/app/index.html#',
 
- 	allScriptsTimeout: 20000,
-
     //baseUrl: 'http://localhost:9000',
+
+    //allScriptsTimeout: 20000,
+
+    //getPageTimeout: 20000,
+
 
 	// Options to be passed to Jasmine-node.
 	jasmineNodeOpts : {
 		showColors : true,
-		defaultTimeoutInterval : 50000
+		defaultTimeoutInterval : 35000
 	}
-	
-	
 };
