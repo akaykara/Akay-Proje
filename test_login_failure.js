1e392556c1a6b5de02f6b59cd616c99d257c2434
akaykara <akay_kara@hotmail.com>
2014-08-11 19:19:21 +0300

    Update test_login_failure.js

--- test_login_failure.js
+++ test_login_failure.js
@@ -13,13 +13,58 @@ it("Selenium Test", function() {
         ptor.sleep(2000);
 
     });
+    
+    it("Register", function() {
+       // ptor.get('http://localhost:63342/BespokeCloud/app/index.html');
+        ptor.get('http://188.226.206.224/app');
+        ptor.sleep(4000);
+
+        element.all(by.id('navRegister')).then(function(selectItem) {
+            selectItem[0].click(); //will click on first item
+
+            var username = ptor.findElement(protractor.By.model('registerObj.username'));
+            username.sendKeys('akay');
+
+            var realname = ptor.findElement(protractor.By.model('registerObj.name'));
+            realname.sendKeys('kara');
+
+            var password = ptor.findElement(protractor.By.model('registerObj.password'));
+            password.sendKeys('12345');
+
+            var password2 = ptor.findElement(protractor.By.model('registerObj.password2'));
+            password2.sendKeys('12345');
+
+            var email = ptor.findElement(protractor.By.model('registerObj.email'));
+            email.sendKeys('akay_kara@hotmail.com');
+
+            element.all(by.id('role')).then(function(selectItem) {
+                selectItem[0].click(); //will click on first item
+                element(by.css('option[value=1]'));
+                ptor.sleep(1000);
+
+            expect(username.getAttribute('value')).toEqual('akay');
+            expect(realname.getAttribute('value')).toEqual('kara');
+            expect(password.getAttribute('value')).toEqual('12345');
+            expect(password2.getAttribute('value')).toEqual('12345');
+            expect(email.getAttribute('value')).toEqual('akay_kara@hotmail.com');
+
+            element.all(by.id('register2')).then(function(selectItem) {
+               selectItem[0].click(); //will click on first item
+            ptor.sleep(1500);
+
+        });
+    });
+});
+});
+    
+    
 
 
     it("Drawing Test", function() {
     	
        // ptor.get('http://localhost:63342/BespokeCloud/app/index.html');
 
- ptor.get('http://188.226.206.224/app/#/connect');
+ ptor.get('http://188.226.206.224/app');
         ptor.sleep(5000);
 
         element.all(by.id('drawing')).then(function(selectItem) {
